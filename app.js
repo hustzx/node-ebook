82ddb2ff9541134b5c8b0dd6a666a593d7d4e7c2
奋斗的胖轩 <2737292143@qq.com>
2019-03-11 21:13:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -11,10 +11,7 @@ app.use(cors());
  // 这个handler函数会接收req和res两个对象，他们分别是请求的 request 和 response。
  // request中包含了浏览器传来的各种信息，比如 query，body，headers之类的，都可以通过req对象访问到。
  // res 对象，我们一般不从里面取信息，而是通过它来定制我们向浏览器输出的信息
- // 比如 header 信息，比如想要向浏览器输出的内容。这里我们调用了它的#send方法，向浏览器输出一个字符串。
- app.get('/', function (req, res) {
-    res.send(new Date().toDateString())
- });
+ 
   // 连接数据库方法
  function connect () {
      return mysql.createConnection({
